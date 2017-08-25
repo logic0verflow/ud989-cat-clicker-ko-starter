4c7fb4a62ec2fd70a8de84da2228c27c99567c9e
logic0verflow <git@nathanacosta.com>
2017-08-24 22:20:23 -0600

    Updated with cat list and cat changing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,28 @@
+var initialCats = [
+    {
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/big',
+        nickNames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
+    },{
+        clickCount: 0,
+        name: 'Paul',
+        imgSrc: 'img/22252709_010df3379e_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/big',
+        nickNames: ['Shore']
+    },{
+        clickCount: 0,
+        name: 'Squints',
+        imgSrc: 'img/9648464288_2516b35537_z.jpg',
+        imgAttribution: 'https://www.flickr.com/photos/big',
+        nickNames: ['Sleepy']
+    }
+];
+
 var Cat = function (data) {
+    var self = this;
+
     this.clickCount = ko.observable(data.clickCount);
     this.name = ko.observable(data.name);
     this.imgSrc = ko.observable(data.imgSrc);
@@ -18,13 +42,17 @@ var Cat = function (data) {
 var ViewModel = function() {
     var self = this;
 
-    this.currentCat = ko.observable( new Cat({
-        clickCount: 0,
-        name: 'Tabby',
-        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-        imgAttribution: 'https://www.flickr.com/photos/big',
-        nickNames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
-    }) );
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem) {
+        self.catList.push( new Cat(catItem) );
+    });
+
+    this.currentCat = ko.observable( this.catList()[0] );
+
+    self.changeCat = function(catSelected) {
+        self.currentCat(catSelected);
+    };
 
     this.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
